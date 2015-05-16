581f35ab18c72df2b75dadbd641d3d25c7ed7a75
I. Lucut <ioan.lucut88@gmail.com>
2015-05-16 22:00:10 +0200

    feat(payment) - Add customer id to perform transaction operation

--- resources/src/main/java/com/revaluate/resource/payment/PaymentResource.java
+++ resources/src/main/java/com/revaluate/resource/payment/PaymentResource.java
@@ -4,10 +4,10 @@ import com.braintreegateway.Result;
 import com.braintreegateway.Transaction;
 import com.revaluate.resource.utils.Resource;
 import com.revaluate.resource.utils.Responses;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,7 +26,7 @@ public class PaymentResource extends Resource {
     // Sub paths
     //-----------------------------------------------------------------
     private static final String FETCH_TOKEN = "fetchToken/{customerId}";
-    private static final String PERFORM_PAYMENT = "performPayment/{paymentNonce}";
+    private static final String PERFORM_PAYMENT = "performPayment/{customerId}/{paymentNonce}";
 
     //-----------------------------------------------------------------
     // Path params
@@ -41,7 +41,7 @@ public class PaymentResource extends Resource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_JSON})
     @Path(FETCH_TOKEN)
-    public Response fetchToken(@PathParam(CUSTOMER_ID) @NotNull String customerId) throws PaymentException {
+    public Response fetchToken(@PathParam(CUSTOMER_ID) @NotEmpty String customerId) throws PaymentException {
         String token = paymentService.fetchToken(customerId);
 
         return Responses.respond(Response.Status.OK, token);
@@ -51,8 +51,8 @@ public class PaymentResource extends Resource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_JSON})
     @Path(PERFORM_PAYMENT)
-    public Response performPayment(@PathParam(PAYMENT_NONCE) @NotNull String paymentNonce) throws PaymentException {
-        Result<Transaction> pay = paymentService.pay(BigDecimal.ONE, paymentNonce);
+    public Response performPayment(@PathParam(CUSTOMER_ID) @NotEmpty String customerId, @PathParam(PAYMENT_NONCE) @NotEmpty String paymentNonce) throws PaymentException {
+        Result<Transaction> pay = paymentService.pay(BigDecimal.ONE, customerId, paymentNonce);
         if (!pay.isSuccess()) {
 
             throw new PaymentException("Error while trying to perform payment.");
