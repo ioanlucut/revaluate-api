a867435f33d63fcfaabb1593ed9d1c506035e584
I. Lucut <ioan.lucut88@gmail.com>
2015-05-18 12:38:13 +0200

    feat(payment) - Payment resource.

--- resources/src/main/java/com/revaluate/resource/payment/PaymentResource.java
+++ resources/src/main/java/com/revaluate/resource/payment/PaymentResource.java
@@ -1,17 +1,21 @@
 package com.revaluate.resource.payment;
 
-import com.braintreegateway.Result;
-import com.braintreegateway.Transaction;
+import com.revaluate.domain.payment.PaymentDetailsDTO;
+import com.revaluate.domain.payment.PaymentStatusDTO;
+import com.revaluate.payment.exception.PaymentStatusException;
+import com.revaluate.payment.service.PaymentStatusService;
 import com.revaluate.resource.utils.Resource;
 import com.revaluate.resource.utils.Responses;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.*;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.math.BigDecimal;
 
 @Path(PaymentResource.PAYMENT)
 @Component
@@ -25,40 +29,37 @@ public class PaymentResource extends Resource {
     //-----------------------------------------------------------------
     // Sub paths
     //-----------------------------------------------------------------
-    private static final String FETCH_TOKEN = "fetchToken/{customerId}";
-    private static final String PERFORM_PAYMENT = "performPayment/{customerId}/{paymentNonce}";
-
-    //-----------------------------------------------------------------
-    // Path params
-    //-----------------------------------------------------------------
-    private static final String CUSTOMER_ID = "customerId";
-    private static final String PAYMENT_NONCE = "paymentNonce";
+    private static final String FETCH_TOKEN = "fetchToken";
+    private static final String CREATE_PAYMENT_STATUS = "createPaymentStatus";
 
     @Autowired
     private PaymentService paymentService;
+
+    @Autowired
+    private PaymentStatusService paymentStatusService;
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_JSON})
     @Path(FETCH_TOKEN)
-    public Response fetchToken(@PathParam(CUSTOMER_ID) @NotEmpty String customerId) throws PaymentException {
-        String token = paymentService.fetchToken(customerId);
+    public Response fetchToken() throws PaymentStatusException {
+        PaymentStatusDTO paymentStatus = paymentStatusService.findOneByUserId(getCurrentUserId());
 
-        return Responses.respond(Response.Status.OK, token);
+        try {
+            return Responses.respond(Response.Status.OK, paymentService.fetchToken(paymentStatus.getCustomerId()));
+        } catch (PaymentException ex) {
+
+            throw new PaymentStatusException(ex);
+        }
     }
 
-    /*@POST
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_JSON})
-    @Path(PERFORM_PAYMENT)
-    public Response performPayment(@PathParam(CUSTOMER_ID) @NotEmpty String customerId, @PathParam(PAYMENT_NONCE) @NotEmpty String paymentNonce) throws PaymentException {
-        Result<Transaction> pay = paymentService.pay(BigDecimal.ONE, customerId, paymentNonce);
-        if (!pay.isSuccess()) {
+    @Path(CREATE_PAYMENT_STATUS)
+    public Response create(@Valid PaymentDetailsDTO paymentDetailsDTO) throws PaymentStatusException {
+        PaymentStatusDTO paymentStatus = paymentStatusService.createPaymentStatus(paymentDetailsDTO, getCurrentUserId());
 
-            throw new PaymentException("Error while trying to perform payment.");
-        }
-
-        return Responses.respond(Response.Status.OK, pay.getMessage());
-    }*/
-
+        return Responses.respond(Response.Status.OK, "Payment status successful created!");
+    }
 }
