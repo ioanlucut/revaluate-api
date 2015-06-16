89653e22875794e1f4ebfa00d01ba4e7475bc741
I. Lucut <ioan.lucut88@gmail.com>
2015-06-16 20:41:56 +0200

    refactor() - Send from backend some important configuration stuff before angular is started. Really cool, I know.

--- resources/src/main/java/com/revaluate/resource/config/AppConfigResource.java
+++ resources/src/main/java/com/revaluate/resource/config/AppConfigResource.java
@@ -1,14 +1,13 @@
 package com.revaluate.resource.config;
 
 import com.revaluate.account.exception.UserException;
+import com.revaluate.color.service.ColorService;
 import com.revaluate.config.AppConfig;
 import com.revaluate.core.annotations.Public;
-import com.revaluate.domain.settings.AppConfigDTO;
-import com.revaluate.domain.settings.AppConfigDTOBuilder;
-import com.revaluate.domain.settings.KeyValueDTO;
-import com.revaluate.domain.settings.KeyValueDTOBuilder;
+import com.revaluate.domain.color.ColorDTO;
 import com.revaluate.resource.utils.Resource;
 import com.revaluate.resource.utils.Responses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.Consumes;
@@ -18,7 +17,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Path(AppConfigResource.APP_CONFIG)
 @Component
@@ -27,21 +28,34 @@ public class AppConfigResource extends Resource {
     public static final String APP_CONFIG = "appconfig";
     private static final String FETCH_CONFIG = "fetchConfig";
 
+    @Autowired
+    private ColorService colorService;
+
     @GET
     @Public
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes({MediaType.APPLICATION_JSON})
     @Path(FETCH_CONFIG)
     public Response isUnique() throws UserException {
+        Map<String, Object> appConfig = new HashMap<>();
 
-        List<KeyValueDTO> keyValueDTOs = Arrays.asList(new KeyValueDTOBuilder().withKey("TRIAL_DAYS").withValue(String.valueOf(AppConfig.TRIAL_DATE)).build());
+        appConfig.put("VERSION", AppConfig.VERSION);
+        appConfig.put("TRIAL_DAYS", AppConfig.TRIAL_DATE);
 
-        AppConfigDTO appConfigDTO = new AppConfigDTOBuilder()
-                .withVersion(AppConfig.VERSION)
-                .withKeyValueDTOList(keyValueDTOs)
-                .build();
+        //-----------------------------------------------------------------
+        // Put also all colors
+        //-----------------------------------------------------------------
+        List<ColorDTO> allColors = colorService.findAllColors();
+        appConfig.put("ALL_COLORS", allColors);
 
-        return Responses.respond(Response.Status.OK, appConfigDTO);
+        //-----------------------------------------------------------------
+        // Put also all predefined categories
+        //-----------------------------------------------------------------
+        List<String> predefinedCategories = Arrays.asList("Bills", "Food", "Clothes", "Car", "Donations", "Hobby", "Health", "Education", "Investments", "House");
+        appConfig.put("PREDEFINED_CATEGORIES", predefinedCategories);
+
+
+        return Responses.respond(Response.Status.OK, appConfig);
     }
 
 }
