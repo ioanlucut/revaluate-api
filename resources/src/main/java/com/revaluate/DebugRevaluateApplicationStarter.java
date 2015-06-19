0fbedc9e03ae06c0c81e17dbb9e6776df371c26a
I. Lucut <ioan.lucut88@gmail.com>
2015-06-19 21:45:29 +0200

    feat() - Subscription cannot be activated while user is in trial

--- resources/src/main/java/com/revaluate/DebugRevaluateApplicationStarter.java
+++ resources/src/main/java/com/revaluate/DebugRevaluateApplicationStarter.java
@@ -11,6 +11,6 @@ public class DebugRevaluateApplicationStarter {
         Optional<URL> urlOptional = Optional.ofNullable(DebugRevaluateApplicationStarter.class.getClassLoader().getResource("config_local.yaml"));
         System.setProperty(ConfigProperties.ENVIRONMENT, "local");
 
-        new RevaluateApplication().run("server", urlOptional.orElseThrow(Exception::new).getPath());
+        new RevaluateApplication().run("server" , urlOptional.orElseThrow(Exception::new).getPath());
     }
 }
